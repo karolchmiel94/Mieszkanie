7bd0010113d377acdeaf05ef0906c35956f4992c
karol <karolch94>
2017-08-23 12:17:32 +0200

    Stage 0

---
+++ app/src/main/java/chmiel/karol/mieszkanie/Controllers/ShoppingList.java
@@ -0,0 +1,30 @@
+package chmiel.karol.mieszkanie.Controllers;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import chmiel.karol.mieszkanie.R;
+
+public class ShoppingList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Item added", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
